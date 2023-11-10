15b555b1ef8566e95e646f81b3ac9e06b4232a38
Stas Evdok <e118sa4002@gmail.com>
2023-11-10 20:01:35 +0300

    start

---
+++ modules/button-link.js
@@ -0,0 +1,24 @@
+let buttonLink = document.querySelector('#button-link');
+
+function copyToClipboard() {
+    navigator.clipboard.writeText('https://stasevdok.github.io/buhlometr/');
+    let oldInnerHtml = buttonLink.innerHTML;
+    buttonLink.innerHTML = 'Ссылка скопирована';
+    setTimeout(function() {
+        buttonLink.innerHTML = oldInnerHtml;
+      }, 1000);
+};
+
+buttonLink.addEventListener('click', function() {
+    copyToClipboard();
+});
+
+function changeButtonText() {
+    if (window.innerWidth < 500) {
+        buttonLink.innerHTML = 'Ссылка<img class="button-icon" src="i/link.svg">';
+    } else {
+        buttonLink.innerHTML = 'Ссылка на калькулятор<img class="button-icon" src="i/link.svg">';
+    };
+};
+
+changeButtonText();
